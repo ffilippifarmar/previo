e73f180d6202b959cb57d297d27f7113fe3ea35d
ffilippifarmar <70654853+ffilippifarmar@users.noreply.github.com>
2022-02-10 20:58:04 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, {Component} from 'react'
 import Pokemons from './components/pokemons'
+import Login from  './components/login'
+import { BrowserRouter as Router, Navigate, Route,Routes } from 'react-router-dom';
+
 
 class App extends Component {
+  
+  state = {
+    isAuth: false
+  }
+  
+  login(){
+    if(this.state.isAuth){
+      <Navigate to="/dashboard"/>
+    }
+    
+  }
+
   render(){
       return (
-        <div>
-          <header className="App-header">
-            <Pokemons/>
-          </header>
-        </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<div><Login logged={this.state.isAuth}/></div>}/>
+            <Route path="/dashboard" element={<div><Pokemons logged={this.state.isAuth}/></div>}/>
+          </Routes>
+        </Router>
       )
   }
 }
